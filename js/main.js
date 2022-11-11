35b0b3fbce3a80d5d77b04b14b2ede871d7ac5f8
pnguyen121 <phillynguyen17@gmail.com>
2022-11-11 11:41:55 -0500

    Updated UI + JS

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 /*----- constants -----*/
 
+const cards = []
 
   /*----- state variables -----*/
-
+let playerCards;
+let computerCards;
+let wins;
+let draws;
+let losses;
 
   /*----- cached elements  -----*/
+    // Selecting the Buttons from DOM
+const hitBtn = document.querySelector('#hit-btn')
+const betBtn = document.querySelector('#bet-btn')
+const standBtn = document.querySelector('#stand-btn')
+    // Selecting Scores from DOM
+const winScoreEl = document.querySelector('#wins-scores')
+const lossScoreEl = document.querySelector('#loss-scores')
+const  drawScoreEl = document.querySelector('#draw-scores')
 
 
   /*----- event listeners -----*/
